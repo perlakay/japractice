87f0e2e8a1737fd31f984e101c290ad8bfb8b3a8
perlakay <perla.kay22@gmail.com>
2025-01-27 14:48:51 -0500

    updates

--- JSlearning/controlflow.js
+++ JSlearning/controlflow.js
@@ -27,6 +27,68 @@ for (let num = 0; num < 100; num++){
     console.log(num) //prints numbers 0-100
 }
 
+let id = 0;
+while(id < numbers.length){
+    console.log(numbers[id]);
+
+    id++;
+}
+
+let sum = 0;
+
+while(true){
+    console.log('loop');
+    sum++;
+
+    if(sum === 10)
+        break; //break case so its not an infinite loop
+}
+
+//you can use a for loop when you have a spcific lnegth or iterations
+//while loops are good for while a ocndition is true
+
+//ternary operators
+
+let age = 16;
+const canDrive = age >= 16 ? true : false 
+
+
+function maxNum(num1,num2){
+    return num1 >= num2 ? num1 : num2;
+}
+//returns max number 
+
+function FizzBuzz(num){
+    if (num % 3 === 0 && num % 5 === 0)
+        return "FizzBuzz";
+    else if (num % 3 === 0)
+        return "Fizz";
+    else if (num % 5 === 0)
+        return "Buzz";
+    else 
+        return num; 
+
+}
+
+console.log(FizzBuzz());
+
+function EvenNum(array){
+    for(const number of array){
+        if(number % 2 === 0)
+            console.log(number);
+    }
+}
+
+function OddNum(array){
+    for(const number of array){
+        if(number % 2 != 0)
+            console.log(number);
+    }
+}
+
+
+
+
 
 
 
--- RockPaperScissors/app.js
+++ RockPaperScissors/app.js
@@ -1,9 +1,62 @@
-const computerChoice = document.getElementById('computer-choice')
-const userChoice = document.getElementById('user-choice')
-const resultDisplay = document.getElementById('result')
+const computerChoiceDisplay = document.getElementById('computer-choice');
+const userChoiceDisplay = document.getElementById('user-choice');
+const resultDisplay = document.getElementById('result');
+let userChoice;
+let computerChoice;
+let result 
 
- const possibleChoices = document.querySelectorAll('button')
+const possibleChoices = document.querySelectorAll('button');
 
- possibleChoices.forEach(possibleChoice => possibleChoice.addEventListener('click',() => {
+possibleChoices.forEach(possibleChoice => possibleChoice.addEventListener('click', (e) => {
+    userChoice = e.target.id;
+    userChoiceDisplay.innerHTML = userChoice;
+    generateComputerChoice();
+    getResult();
+}));
+
+function generateComputerChoice() {
+    const randomNumber = Math.floor(Math.random() * 3) + 1;
+    console.log(randomNumber); // Debugging step
+
+    // Assign computer choice based on the random number
+    if (randomNumber === 1) {
+        computerChoice = 'rock';
+    } else if (randomNumber === 2) {
+        computerChoice = 'scissors';
+    } else if (randomNumber === 3) {
+        computerChoice = 'paper';
+    }
+
+    // Update the display
+    computerChoiceDisplay.innerHTML = computerChoice;
+}
+
+function getResult(){
+    if (computerChoice === userChoice){
+        result = "its a draw"
+    }
+    if (computerChoice === 'rock' && userChoice === 'paper'){
+        result = "you win!"
+    }
+    if (computerChoice === 'rock' && userChoice === 'scissors'){
+        result = "you lost"
+   
+    }
+    if (computerChoice === 'paper' && userChoice === 'scissors'){
+        result = "you win!"
+    }
+
+    if (computerChoice === 'paper' && userChoice === 'rock'){
+        result = "you lost"
+    }
+    if (computerChoice === 'scissors' && userChoice === 'rock'){
+        result = "you win!"
+    }
+    if (computerChoice === 'scissors' && userChoice === 'paper'){
+        result = "you lost"
+    }
     
- }))
+    resultDisplay.innerHTML = result;
+
+
+}
